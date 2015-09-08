be572ec9d3a8abe2984e287df9a77227032e04c5
Sarah <sarah.grefalda@apexlearning.com>
2015-09-08 16:00:23 -0700

    Adding more sample content

--- samples/theNose.js
+++ samples/theNose.js
@@ -40,6 +40,87 @@ define({
                 ],
                 [
                     'He put in his finger, and drew out -- a nose!'
+                ],
+                [
+                    'Ivan Jakovlevitch at first let his hands fall from sheer astonishment; then he rubbed his eyes and began to feel it. A nose, an actual nose; and, moreover, it seemed to be the nose of an acquaintance! Alarm and terror were depicted in Ivan\'s face; but these feelings were slight in comparison with the disgust which took possession of his wife.'
+                ],
+                [
+                    '"Whose nose have you cut off, you monster?" she screamed, her face red with anger. "You scoundrel! You tippler! I myself will report you to the police! Such a rascal! Many customers have told me that while you were shaving them, you held them so tight by the nose that they could hardly sit still."'
+                ],
+                [
+                    'But Ivan Jakovlevitch was more dead than alive; he saw at once that this nose could belong to no other than to Kovaloff, a member of the Municipal Committee whom he shaved every Sunday and Wednesday.'
+                ],
+                [
+                    '"Stop, Prasskovna Ossipovna! I will wrap it in a piece of cloth and place it in the corner. There it may remain for the present; later on I will take it away."'
+                ],
+                [
+                    '"No, not there! Shall I endure an ',
+                    {
+                        text: 'amputated',
+                        question: 'Define "amputated".'
+                    },
+                    ' nose in my room? You understand nothing except how to strop a razor. You know nothing of the duties and obligations of a respectable man. You vagabond! You good-for-nothing! Am I to undertake all responsibility for you at the police-office? Ah, you soap-smearer! You blockhead! Take it away where you like, but don\'t let it stay under my eyes!"'
+                ],
+                [
+                    'Ivan Jakovlevitch stood there flabbergasted. He thought and thought, and knew not what he thought.'
+                ],
+                [
+                    '"The devil knows how that happened!" he said at last, scratching his head behind his ear. '
+                ],
+                [
+                    '"Whether I came home drunk last night or not, I really don\'t know; but in all probability this is a quite extraordinary occurrence, for a loaf is something baked and a nose is something different. I don\'t understand the matter at all." And Ivan Jakovlevitch was silent. The thought that the police might find him in unlawful possession of a nose and arrest him, robbed him of all presence of mind. Already he began to have visions of a red collar with silver braid and of a sword — and he trembled all over.'
+                ],
+                [
+                    'At last he finished dressing himself, and to the accompaniment of the emphatic exhortations of his spouse, he wrapped up the nose in a cloth and issued into the street.'
+                ],
+                [
+                    'He intended to lose it somewhere — either at somebody\'s door, or in a public square, or in a narrow alley; but just then, in order to complete his bad luck, he was met by an acquaintance, who showered inquiries upon him. "Hullo, Ivan Jakovlevitch! Whom are you going to shave so early in the morning?" etc., so that he could find no suitable opportunity to do what he wanted. Later on he did let the nose drop, but a sentry bore down upon him with his halberd, and said, "Look out! You have let something drop!" and Ivan Jakovlevitch was ',
+                    {
+                        text: 'obliged',
+                        question: 'Define "obliged".'
+                    },
+                    ' to pick it up and put it in his pocket.'
+                ],
+                [
+                    'A feeling of despair began to take possession of him; all the more as the streets became more thronged and the merchants began to open their shops. At last he resolved to go to the Isaac Bridge, where perhaps he might succeed in throwing it into the Neva.'
+                ],
+                [
+                    'But my conscience is a little uneasy that I have not yet given any detailed information about Ivan Jakovlevitch, an estimable man in many ways.'
+                ],
+                [
+                    'Like every honest Russian tradesman, Ivan Jakovlevitch was a terrible drunkard, and although he shaved other people\'s faces every day, his own was always unshaved. His coat (he never wore an overcoat) was quite mottled, i.e. it had been black, but become brownish-yellow; the collar was quite shiny, and instead of the three buttons, only the threads by which they had been fastened were to be seen.'
+                ],
+                [
+                    'Ivan Jakovlevitch was a great ',
+                    {
+                        text: 'cynic',
+                        question: 'Define "cynic".'
+                    },
+                    ', and when Kovaloff, the member of the Municipal Committee, said to him, as was his custom while being shaved, "Your hands always smell, Ivan Jakovlevitch!" the latter answered, "What do they smell of?" "I don\'t know, my friend, but they smell very strong." Ivan Jakovlevitch after taking a pinch of snuff would then, by way of reprisals, set to work to soap him on the cheek, the upper lip, behind the ears, on the chin, and everywhere.'
+                ],
+                [
+                    'This worthy man now stood on the Isaac Bridge. At first he looked round him, then he leant on the railings of the bridge, as though he wished to look down and see how many fish were swimming past, and secretly threw the nose, wrapped in a little piece of cloth, into the water. He felt as though a ton weight had been lifted off him, and laughed cheerfully. Instead, however, of going to shave any officials, he turned his steps to a building, the sign-board of which bore the legend "Teas served here," in order to have a glass of punch, when suddenly he perceived at the other end of the bridge a police inspector of imposing exterior, with long whiskers, three-cornered hat, and sword hanging at his side. He nearly fainted; but the police inspector beckoned to him with his hand and said, "Come here, my dear sir."'
+                ],
+                [
+                    'Ivan Jakovlevitch, knowing how a gentleman should behave, took his hat off quickly, went towards the police inspector and said, "I hope you are in the best of health."'
+                ],
+                [
+                    '"Never mind my health. Tell me, my friend, why you were standing on the bridge."'
+                ],
+                [
+                    '"By heaven, gracious sir, I was on the way to my customers, and only looked down to see if the river was flowing quickly."'
+                ],
+                [
+                    '"That is a lie! You won\'t get out of it like that. Confess the truth."'
+                ],
+                [
+                    '"I am willing to shave Your Grace two or even three times a week gratis," answered Ivan Jakovlevitch.'
+                ],
+                [
+                    '"No, my friend, don\'t put yourself out! Three barbers are busy with me already, and reckon it a high honour that I let them show me their skill. Now then, out with it! What were you doing there?"'
+                ],
+                [
+                    'Ivan Jakovlevitch grew pale. But here the strange episode vanishes in mist, and what further happened is not known.'
                 ]
             ]
         }
